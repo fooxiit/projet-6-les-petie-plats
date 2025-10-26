47cdc4fe663da69d683d4b67e13b218782ad978e
guillian <guillian.edo@gmail.com>
2025-10-26 02:12:19 +0200

    feat: refactor displayRecipes function to include search term and update parameters

--- javascript/function/displayRecipes.js
+++ javascript/function/displayRecipes.js
@@ -1,13 +1,23 @@
 import { Recipe } from '../class/recipe.js';
+import { parseHttml } from './parseHtml.js';
 
 /**
  *
- * @param {HTMLElement} anchor
  * @param {Array<Recipe>} recipes
+ * @param {HTMLElement} recipesContainer
+ * @param {HTMLElement} numberContainer
  */
-export function displayRecipes(anchor, recipes) {
-    anchor.innerHTML = '';
-    for (const recipe of recipes) {
-        anchor.appendChild(recipe.DOM);
+export function displayRecipes(recipes, recipesContainer, numberContainer, searchTerm = '') {
+    recipesContainer.innerHTML = '';
+    if (recipes.length > 0) {
+        for (const recipe of recipes) {
+            recipesContainer.appendChild(recipe.DOM);
+        }
+    } else {
+        const notFound = parseHttml(`<span> Aucune recette ne contient « ${searchTerm} »  vous pouvez chercher «tarte aux pommes », « poisson », etc</<span>`);
+        recipesContainer.appendChild(notFound);
     }
+    numberContainer.innerHTML = '';
+    const numberOfRecipes = parseHttml(`<span>${recipes.length} recettes</span>`);
+    numberContainer.appendChild(numberOfRecipes);
 }
--- javascript/index.js
+++ javascript/index.js
@@ -83,32 +83,10 @@ const searchBar = new SearchBar({
                 })
             )
         );
-        displayRecipes(recipesContainer, recipes);
+        displayRecipes(recipes, recipesContainer, numberContainer, searchTerm.query);
     },
     className: 'search-bar--main',
 });
 searchBarAchor.appendChild(searchBar.DOM);
 
-displayRecipes(recipesContainer, recipes);
 displayRecipes(recipes, recipesContainer, numberContainer);
-
-/**
- *
- * @param {Array<Recipe>} recipes
- * @param {HTMLElement} recipesContainer
- * @param {HTMLElement} numberContainer
- */
-function displayRecipes(recipes, recipesContainer, numberContainer, searchTerm = '') {
-    recipesContainer.innerHTML = '';
-    if (recipes.length > 0) {
-        for (const recipe of recipes) {
-            recipesContainer.appendChild(recipe.DOM);
-        }
-    } else {
-        const notFound = parseHttml(`<span> Aucune recette ne contient « ${searchTerm} »  vous pouvez chercher «tarte aux pommes », « poisson », etc</<span>`);
-        recipesContainer.appendChild(notFound);
-    }
-    numberContainer.innerHTML = '';
-    const numberOfRecipes = parseHttml(`<span>${recipes.length} recettes</span>`);
-    numberContainer.appendChild(numberOfRecipes);
-}
